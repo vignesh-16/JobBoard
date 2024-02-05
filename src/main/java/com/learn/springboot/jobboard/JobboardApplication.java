800969ac31170861fd6b3da144edc93360b9984b
Vignesh A <assassincreedlll07@gmail.com>
2024-02-05 21:25:00 +0530

    created a java spring boot app using maven as dependency manager with mongodb dependency included

---
+++ src/main/java/com/learn/springboot/jobboard/JobboardApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobboardApplication.class, args);
+	}
+
+}
